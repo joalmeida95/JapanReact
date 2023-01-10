425bae6cf9c529abd638587a2ab19be5acddac0c
joalmeida95 <jmalmeida1995@gmail.com>
2023-01-10 19:28:30 -0300

    link .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ export default function  Home () {
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque nisi sit molestias laborum, cumque exercitationem vitae est amet eius ratione aspernatur suscipit repellendus architecto at beatae repellat sunt officiis consequatur?</p>                     
             
-            <a href="#" class="btn">Explore</a>
+            <a href="." className="btn">Explore</a>
             
                        
         </div>
