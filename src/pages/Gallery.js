55de9576f5fd1cd19e527690900c868fcdbe13a4
joalmeida95 <jmalmeida1995@gmail.com>
2023-01-17 22:37:14 -0300

    @query.768

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -5,27 +5,27 @@ export default function Gallery () {
         <div className='galeria-paisajes'>
             <div className='paisajes'>
                 <h3 className='titulo-h3'>Castillo de Osaka</h3>
-                <img src={require('./images/toma1.jpg')} alt="Castillo-Osaka"/>
+                <img className='img-paisaje' src={require('./images/toma1.jpg')} alt="Castillo-Osaka"/>
             </div>
             <div className='paisajes'>
             <h3 className='titulo-h3'>Mercado de Kuromon</h3>
-                <img src={require('./images/toma2.jpg')} alt="Mercado-Kuromon"/>
+                <img className='img-paisaje' src={require('./images/toma2.jpg')} alt="Mercado-Kuromon"/>
             </div>
             <div className='paisajes'>
             <h3 className='titulo-h3'>Barrio de Doton-bori</h3>
-                <img src={require('./images/toma9.jpg')} alt="Barrio-Doton"/>
+                <img className='img-paisaje' src={require('./images/toma9.jpg')} alt="Barrio-Doton"/>
             </div>
             <div className='paisajes'>
             <h3 className='titulo-h3'>Templo Kiyomizu-dera</h3>               
-                <img src={require('./images/toma3.jpg')} alt="Templo-kiyomizu"/>
+                <img className='img-paisaje' src={require('./images/toma3.jpg')} alt="Templo-kiyomizu"/>
             </div>
             <div className='paisajes'>
             <h3 className='titulo-h3'>Zona de Arashiyama</h3>
-                <img src={require('./images/toma6.jpg')} alt="Arashimaya"/>
+                <img className='img-paisaje' src={require('./images/toma6.jpg')} alt="Arashimaya"/>
             </div>
             <div className='paisajes'>
             <h3 className='titulo-h3'>Templo de Todai-ji</h3>
-            <img src={require('./images/toma10.jpg')} alt="Buda"/>
+                <img className='img-paisaje' src={require('./images/toma10.jpg')} alt="Buda"/>
             </div>
 
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,41 +31,39 @@ export default function  Home () {
       >
         <SwiperSlide>
             <div className='slide1'>
-              <img src={require('./images/toma6.jpg')} alt="Arashimaya"/>
+              <img className='img-slide' src={require('./images/toma6.jpg')} alt="Arashimaya"/>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className='slide2'>
-              <img src={require('./images/toma7.jpg')} alt="Arashimaya"/>
+              <img className='img-slide' src={require('./images/toma3.jpg')} alt="Arashimaya"/>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className='slide3'>
-              <img src={require('./images/toma8.jpg')} alt="Arashimaya"/>
+              <img className='img-slide' src={require('./images/toma8.jpg')} alt="Arashimaya"/>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className='slide4'>
-              <img src={require('./images/toma9.jpg')} alt="Arashimaya"/>
+              <img className='img-slide' src={require('./images/toma9.jpg')} alt="Arashimaya"/>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className='slide5'>
-              <img src={require('./images/toma10.jpg')} alt="Arashimaya"/>
+              <img className='img-slide' src={require('./images/toma10.jpg')} alt="Arashimaya"/>
             </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className='slide6'>
-              <img src={require('./images/toma2.jpg')} alt="Arashimaya"/>
+              <img className='img-slide' src={require('./images/toma2.jpg')} alt="Arashimaya"/>
             </div>
         </SwiperSlide>
         <SwiperSlide>
             <div className='slide7'>
-              <img src={require('./images/toma1.jpg')} alt="Arashimaya"/>
+              <img className='img-slide' src={require('./images/toma1.jpg')} alt="Arashimaya"/>
             </div>
-            </SwiperSlide>
-        {/* <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide> */}
+        </SwiperSlide>
       </Swiper>
             
                        
